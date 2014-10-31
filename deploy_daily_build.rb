4516a2a1daaab008e13709bb8628920d5c58f443
heather-lo <hlo@email.arizona.edu>
2014-10-30 17:01:46 -0700

    adding argument for build number

--- deploy_daily_build.rb
+++ deploy_daily_build.rb
@@ -1,15 +1,22 @@
 # Deploys a daily UAR build
+# The build number comes from either a CI tool or from the command line.
 
 require_relative './lib/daily_build_manager'
 
-S3_BUCKET="katt-packages"
+S3_BUCKET = "katt-packages"
 #S3_BUCKET="kuali-coeus"
 
-#if ARGV.empty?
-#	build_number = "latest"
-#else
-#	build_number = ARGV[0]
-#end
+
+def get_build_number
+	begin
+		if ARGV.empty?
+			raise "Build number is missing"
+		else
+			build_number = ARGV[0]
+			puts "build_number is #{build_number}."
+		end
+	end
+end
 
 build_manager = DailyBuildManager.new(S3_BUCKET)
 
@@ -18,3 +25,6 @@ build_manager.list_all_buckets
 
 puts "contents of #{S3_BUCKET} bucket:"
 build_manager.get_all_daily_packages
+
+puts "getting specific build"
+build_to_deploy = get_build_number
